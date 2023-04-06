36a41b89a6475bba12689055214fa9da8c5c6cbb
SverreWisloff <sverre.wisloeff@gmail.com>
2023-04-06 12:58:48 +0200

    mellomlagring3

--- app/src/main/java/com/example/tackingassist/lineGraph.kt
+++ app/src/main/java/com/example/tackingassist/lineGraph.kt
@@ -18,15 +18,19 @@ class lineGraphView @JvmOverloads constructor(
     ) : View(context, attrs, defStyleAttr) {
 
     private val debug = true
+    private val lineWidth = 18f
     private val fontsize = 60.0f
     private val pathGraph = Path()
     private var size = 0
-    private var graphWidth = 0              // Width of the graph.
-    private var graphHeight = 0
+    private var canvasWidth = 0              // Width of the graph.
+    private var canvasHeight = 0
+    private val inset = 5f                  // Marg=strokeWidth
+    private val leftMargin = 180f           // Venstrmarg. Her vises hjelpe-tall
+    private val rightMargin = 18f          // Høyremarg. Her vises nåværende fart
+    private var plotWidth = 0f
+    private var plotHeight = 0f
     private var graphMinValue = 0.0f
     private var graphMaxValue = 0.0f
-    private val inset = 5f                  // Marg=strokeWidth
-    private val rightMargin = 180f           // Høyremarg. Her vises hjelpe-tall
 
     // Paint styles used for rendering are initialized here. This
     // is a performance optimization, since onDraw() is called
@@ -38,10 +42,10 @@ class lineGraphView @JvmOverloads constructor(
         style = Paint.Style.STROKE // default: FILL
         //strokeJoin = Paint.Join.ROUND // default: MITER
         //strokeCap = Paint.Cap.ROUND // default: BUTT
-        strokeWidth = 15f
+        strokeWidth = lineWidth
     }
     private val paintHelplines = Paint().apply {
-        color = Color.MAGENTA
+        color = Color.WHITE
         style = Paint.Style.STROKE
         strokeWidth = 5f
         //textAlign = Paint.Align.CENTER
@@ -61,16 +65,17 @@ class lineGraphView @JvmOverloads constructor(
      * the view was just added to the view hierarchy, it is called with the old
      * values of 0. The code determines the drawing bounds for the custom view.
      *
-     * @param width    Current width of this view.
+     * @param width     Current width of this view.
      * @param height    Current height of this view.
-     * @param oldWidth Old width of this view.
+     * @param oldWidth  Old width of this view.
      * @param oldHeight Old height of this view.
      */
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         // Calculate the radius from the smaller of the width and height.
-        graphWidth = width
-        graphHeight = height
-
+        canvasWidth  = width
+        canvasHeight = height
+        plotWidth    = canvasWidth - leftMargin - rightMargin - inset - inset
+        plotHeight   = canvasHeight - inset - inset
     }
 
     /**
@@ -87,6 +92,26 @@ class lineGraphView @JvmOverloads constructor(
         Log.d("lineGraphView", "onDraw")
     }
 
+    //   ================================   == I
+    //  ||     |                |       ||
+    //  || 13kn|   _____        |       ||
+    //  ||     |__/     \___    |       ||   H
+    //  ||     |            \___| 12.1kn||
+    //  || 11kn|                |       ||
+    //  ||     |                |       ||
+    //   ================================   == I
+    //
+    //  ||  A  |      B         |  C    ||
+    //  I                                I
+    //
+    // inset        = I
+    // leftMargin   = A
+    // plotWidth    = B     (=canvasWidth-leftMargin-rightMargin-inset-inset)
+    // rightMargin  = C
+    // canvasWidth  = I+A+B+C+I
+    // plotHeight   = H     (=canvasHeight-inset-inset)
+    // canvasHeight = I+H+I
+
     private fun drawHelplines(canvas: Canvas) {
 
         // Fill background color
@@ -95,23 +120,24 @@ class lineGraphView @JvmOverloads constructor(
         // Draw a frame around the canvas.
         //canvas.drawRect( inset, inset , graphWidth-rightMargin, graphHeight-inset, paintHelplines)
 
-        //Draw a help line right over lowest datapont
-        val helplineMin =   floor(graphMinValue).toLong() + 1
-        val helplineMinpix = graphHeight.toFloat() - ((helplineMin - graphMinValue )/(graphMaxValue - graphMinValue) * graphHeight.toFloat())
-        val helplineMinstr = "${helplineMin} kn"
-        canvas.drawLine(0f, helplineMinpix, graphWidth.toFloat()-rightMargin, helplineMinpix, paintHelplines)
-        canvas.drawText( helplineMinstr, graphWidth.toFloat()-rightMargin+20, helplineMinpix, paintHelplines)
-
         //Draw a help line right under top
         val helplineTop =   floor(graphMaxValue).toLong()
-        val helplineToppix = graphHeight.toFloat() - ((helplineTop - graphMinValue )/(graphMaxValue - graphMinValue) * graphHeight.toFloat())
-        val helplineTopstr = "${helplineTop} kn"
-        canvas.drawLine(0f, helplineToppix, graphWidth.toFloat()-rightMargin, helplineToppix, paintHelplines)
-        canvas.drawText( helplineTopstr, graphWidth.toFloat()-rightMargin+20, helplineToppix+fontsize, paintHelplines)
+        val helplineTopPix = canvasHeight.toFloat() - ((helplineTop - graphMinValue )/(graphMaxValue - graphMinValue) * canvasHeight.toFloat())
+        val helplineTopStr = "${helplineTop} kn"
+        canvas.drawLine(leftMargin+inset, helplineTopPix, leftMargin+inset+plotWidth, helplineTopPix, paintHelplines)
+        canvas.drawText( helplineTopStr, inset, helplineTopPix+fontsize, paintHelplines)
+
+        // TODO - if ony one hilplne - dont draw it twice ....
+        //Draw a help line right over lowest datapont
+        val helplineMin =   floor(graphMinValue).toLong() + 1
+        val helplineMinPix = canvasHeight.toFloat() - ((helplineMin - graphMinValue )/(graphMaxValue - graphMinValue) * canvasHeight.toFloat())
+        val helplineMinStr = "${helplineMin} kn"
+        canvas.drawLine(leftMargin+inset, helplineMinPix, leftMargin+inset+plotWidth, helplineMinPix, paintHelplines)
+        canvas.drawText( helplineMinStr, inset, helplineMinPix, paintHelplines)
 
         //TODO - does Log.d run when app is compiled under release?
         if (debug) {
-            val printstring = "helplineMid=${helplineMin} helplineMidpix=${helplineMinpix} helplineTop=${helplineTop} helplineToppix=${helplineToppix}"
+            val printstring = "helplineMin=${helplineMin} helplineMinPix=${helplineMinPix} helplineTop=${helplineTop} helplineTopPix=${helplineTopPix}"
             Log.d("drawHelplines", printstring)
         }
     }
@@ -131,24 +157,27 @@ class lineGraphView @JvmOverloads constructor(
 
         pathGraph.reset() //Clear any lines and curves from the path, making it empty.
 
-        var sinceNow = 0
-        pathGraph.moveTo(inset,graphHeight/2f)
+        var sinceNow = dataPoints-1
 
-        while (sinceNow<dataPoints) {
+        while (sinceNow>=0) {
             val Data = dataBuffer.getData(sinceNow);
 
             // x = Time
-            val x : Float = (((sinceNow.toFloat()+1f )/ dataPoints.toFloat()) * (graphWidth-rightMargin-inset)) + inset
+//            val x : Float = canvasWidth-rightMargin-inset - (((sinceNow.toFloat()+1f )/ dataPoints.toFloat()) * (canvasWidth-rightMargin-inset)) + inset
+            val x : Float = (plotWidth+leftMargin+inset) - (((sinceNow.toFloat()+1f ) / dataPoints.toFloat()) * (plotWidth))
 
             // y = Speed
-            val y : Float = graphHeight.toFloat() - ((Data - graphMinValue )/(graphMaxValue - graphMinValue) * (graphHeight.toFloat()-inset-inset) )
+            val y : Float = canvasHeight.toFloat() - ((Data - graphMinValue )/(graphMaxValue - graphMinValue) * (canvasHeight.toFloat()-inset-inset) )
 
-            pathGraph.lineTo(x,y)
+            if (sinceNow==(dataPoints-1))
+                pathGraph.moveTo(x,y)
+            else
+                pathGraph.lineTo(x,y)
 
-            printstring = "importData x=${x}  y=${y} (i=${sinceNow} dataPoints=${dataPoints} Data=${Data} graphWidth=${graphWidth} graphHeight=${graphHeight} graphMinValue=${graphMinValue} graphMaxValue=${graphMaxValue} )"
+            printstring = "importData x=${x}  y=${y} (i=${sinceNow} dataPoints=${dataPoints} Data=${Data} canvasWidth=${canvasWidth} canvasHeight=${canvasHeight} graphMinValue=${graphMinValue} graphMaxValue=${graphMaxValue} )"
             Log.d("lineGraphView", printstring)
 
-            sinceNow++
+            sinceNow--
         }
 
 
